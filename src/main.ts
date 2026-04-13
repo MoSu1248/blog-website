ee5f0fe012ab48c9f40ee2b9272e55abaa918440
Mohammed <mohammedsuhailr2@gmail.com>
2026-04-13 19:47:53 +0200

    chore: initial project setup with Angular and Tailwind
    - Created Angular project with routing enabled
    - Installed and configured Tailwind CSS
    - Set up base project structure
    - Added global styles and Tailwind directives

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,21 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { Home } from './app/features/home/home';
+import { Blog } from './app/features/blog/blog';
+import { About } from './app/features/about/about';
+import { Newsletter } from './app/features/newsletter/newsletter';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, {
+  providers: [
+    provideRouter([
+      { path: '', component: Home },
+      { path: 'blog', component: Blog },
+      { path: 'about', component: About },
+      { path: 'news letter', component: Newsletter },
+    ]),
+    provideHttpClient(),
+  ],
+});
