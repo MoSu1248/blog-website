2ec1588d72152abeeb144b2921a73492b8083d2d
Mohammed <mohammedsuhailr2@gmail.com>
2026-04-17 11:44:34 +0200

    feat: implement semantic newsletter form and about page sections
    Refactored the about page into distinct semantic sections for the Newsletter and Social Links.
    
    Implemented a functional <form> for the newsletter with type="email" validation and "Enter" key support.
    
    Configured button UI with consistent rounding and blue-500 branding.

--- src/app/core/navbar/navbar.ts
+++ src/app/core/navbar/navbar.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { NgOptimizedImage } from '@angular/common';
+import { Themebtn } from '../../shared/components/themebtn/themebtn';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLinkActive, RouterLink, NgOptimizedImage],
+  imports: [RouterLinkActive, RouterLink, NgOptimizedImage, Themebtn],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {}
-
---
+++ src/app/shared/components/themebtn/themebtn.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { SunIcon } from '../../icons/sun-icon/sun-icon';
+
+@Component({
+  selector: 'app-themebtn',
+  imports: [SunIcon],
+  templateUrl: './themebtn.html',
+  styleUrl: './themebtn.css',
+})
+export class Themebtn {}
