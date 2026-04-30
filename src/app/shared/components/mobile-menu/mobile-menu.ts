0f2d144fc2f85c7be63eca63f5aefdfaa15c72d5
Mohammed <mohammedsuhailr2@gmail.com>
2026-04-30 10:56:02 +0200

    feature: Added mobile menu styling and basic setup
    -added a mobile menu feature and required basic setup for

---
+++ src/app/shared/components/mobile-menu/mobile-menu.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MenuIcon } from '../../icons/menu-icon/menu-icon';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-mobile-menu',
+  imports: [MenuIcon, RouterLinkActive, RouterLink],
+  templateUrl: './mobile-menu.html',
+  styleUrl: './mobile-menu.css',
+})
+export class MobileMenu {}
